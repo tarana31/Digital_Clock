5e9dc3c74136a50581051b11eec1a1faa00b762d
Tarana Patel <tarana3121@gmail.com>
2018-08-02 17:23:21 -0400

    Digital Clock Work

---
+++ digital.js
@@ -0,0 +1,41 @@
+
+function newtime() {
+    var today= new Date();
+    var h= today.getHours();
+    var m= today.getMinutes();
+    var s= today.getSeconds();
+
+    function add_zero(i) {
+        if(i < 10) {
+            i= "0" +i;
+        }
+        return i;
+    }
+    m= add_zero(m);
+    s= add_zero(s);
+
+    document.getElementById('clock').innerHTML = h +":" + m + ":" + s;
+    // console.log(h);
+
+    setTimeout(newtime, 500);
+
+        if(h<12)
+        {
+            document.body.style.backgroundImage= 'url(morining.jpg)';
+            document.getElementById('heading').innerHTML= "Good Morning!";
+        }
+
+        if(h>=12 && h<17) {
+            document.body.style.backgroundImage= 'url(afternoon2.jpg)';
+            document.getElementById('heading').innerHTML= "Good Afternoon!";
+
+        }
+        
+        if(h>=17) {
+            document.body.style.backgroundImage= 'url(evening.jpg)';
+            document.getElementById('heading').innerHTML= "Good Evening!"
+            document.getElementById('heading').style.color = 'white'
+	        document.getElementById('clock').style.color = 'white'
+        }
+}
+newtime();
